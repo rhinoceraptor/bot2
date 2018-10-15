2348703b6636baa9e3a58d9c979053274ce2847b
Jack Lewis <jack@jacklew.is>
2018-10-15 08:00:31 -0400

    Fixed room joining and implemented send_message

--- src/bot.rs
+++ src/bot.rs
@@ -5,17 +5,6 @@ use reqwest;
 use config::*;
 use structures::*;
 
-pub struct Room {
-  pub room: String,
-  pub room_id: String,
-}
-
-impl Room {
-  pub fn new(room: String, room_id: String) -> Self {
-    Room { room, room_id }
-  }
-}
-
 pub struct Bot {
   config: Config,
   access_token: Option<String>,
@@ -32,14 +21,18 @@ impl Bot {
 
     println!("Got auth token");
 
-    //let rooms: Vec<Room> = self.config.rooms
-    //  .iter()
-    //  .map(|room| Room::new(
-    //    room.to_string(),
-    //    self.get_room_id(room.to_string())
-    //      .expect(&format!("Failed to get room id for {}", room))
-    //  ))
-    //  .collect();
+    self.room_list = self.config.rooms
+      .iter()
+      .map(|room| Room::new(
+        room.to_string(),
+        self.join_room(room.to_string())
+          .expect(&format!("Failed to get room id for {}", room))
+      ))
+      .collect();
+
+    for room in &self.room_list {
+      self.send_message(Message::new("wubba lubba dub dub".to_string()), room.room_id.to_string())?;
+    }
 
     Ok(())
   }
@@ -69,43 +62,42 @@ impl Bot {
     Ok(access_token)
   }
 
-  fn get_room_id(&self, room: String) -> Result<String, Box<Error>> {
+  fn join_room(&self, room: String) -> Result<String, Box<Error>> {
     let Authentication { home_server_domain, .. } = &self.config.authentication;
 
-    let access_token = match self.access_token {
-      Some(ref t) => t,
-      None => panic!("Bot not authenticated")
-    };
-
     let uri = self.build_url(&format!(
-      "/directory/{}:{}?access_token={}",
+      "join/{}:{}?access_token={}",
       room,
       home_server_domain,
-      access_token
+      self.access_token.as_ref().unwrap()
     ));
 
-    println!("{}", uri);
     let client = reqwest::Client::new();
-    let body = client
-      .get(&uri)
-      .send()?;
-    println!("{:#?}", body);
+    let RoomId { room_id } = client
+      .post(&uri)
+      .send()?
+      .json()?;
 
-    // let RoomId { room_id, .. } = client
-    //   .get(&uri)
-    //   .query(&[("access_token", &self.access_token)])
-    //   .send()?
-    //   .json()?;
-
-
-    //println!("room: {}, room_id: {}", room, room_id);
-    Ok("test".to_string())
+    Ok(room_id)
   }
 
-  // fn send_message(&self, message, room) -> Result<(), Box<Error>> {
-  //   let uri = format!(
-  //     "{}/_matrix/client/r0/rooms/{}"
-  //   );
-  // }
+  fn send_message(&self, message: Message, room: String) -> Result<(), Box<Error>> {
+    let Authentication { home_server_domain, .. } = &self.config.authentication;
+
+    let uri = self.build_url(&format!(
+      "rooms/{}/send/m.room.message?access_token={}",
+      room,
+      self.access_token.as_ref().unwrap()
+    ));
+
+    let client = reqwest::Client::new();
+    let body = client
+      .post(&uri)
+      .json(&message)
+      .send()?
+      .text()?;
+
+    Ok(())
+  }
 }
 
--- src/structures.rs
+++ src/structures.rs
@@ -37,6 +37,16 @@ impl Message {
 #[derive(Deserialize, Debug)]
 pub struct RoomId {
   pub room_id: String,
-  pub servers: Vec<String>,
 }
 
+#[derive(Debug)]
+pub struct Room {
+  pub room: String,
+  pub room_id: String,
+}
+
+impl Room {
+  pub fn new(room: String, room_id: String) -> Self {
+    Room { room, room_id }
+  }
+}
